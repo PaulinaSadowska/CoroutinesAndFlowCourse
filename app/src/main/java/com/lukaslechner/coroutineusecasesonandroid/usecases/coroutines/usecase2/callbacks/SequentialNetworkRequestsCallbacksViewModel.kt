b540d5f707b446a375b5a70b91c2e8f5d9b4ac77
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-06 19:06:40 +0200

    8. Callback Implementation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
@@ -1,12 +1,68 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.callbacks
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Callback
 
 class SequentialNetworkRequestsCallbacksViewModel(
     private val mockApi: CallbackMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun perform2SequentialNetworkRequest() {
+    var call1: Call<List<AndroidVersion>>? = null
+    var call2: Call<VersionFeatures>? = null
 
+    fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+        call1 = mockApi.getRecentAndroidVersions()
+        call1!!.enqueue(object : Callback<List<AndroidVersion>> {
+            override fun onResponse(
+                p0: Call<List<AndroidVersion>>,
+                response: Response<List<AndroidVersion>>
+            ) {
+                if (response.isSuccessful) {
+                    val lastVersion = response.body()!!.last()
+                    getVersionFeatures(lastVersion)
+                } else {
+                    uiState.value = UiState.Error("not found")
+                }
+
+            }
+
+            override fun onFailure(p0: Call<List<AndroidVersion>>, p1: Throwable) {
+                uiState.value = UiState.Error("not found")
+            }
+
+        })
+    }
+
+    private fun getVersionFeatures(lastVersion: AndroidVersion) {
+        call2 = mockApi.getAndroidVersionFeatures(lastVersion.apiLevel)
+        call2?.enqueue(object : Callback<VersionFeatures> {
+            override fun onResponse(
+                p0: Call<VersionFeatures>,
+                response2: Response<VersionFeatures>
+            ) {
+                if (response2.isSuccessful) {
+                    uiState.value = UiState.Success(response2.body()!!)
+                } else {
+                    uiState.value = UiState.Error("not found")
+                }
+            }
+
+            override fun onFailure(p0: Call<VersionFeatures>, p1: Throwable) {
+                uiState.value = UiState.Error("not found")
+            }
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        call1?.cancel()
+        call2?.cancel()
+        call1 = null
+        call2 = null
     }
 }
