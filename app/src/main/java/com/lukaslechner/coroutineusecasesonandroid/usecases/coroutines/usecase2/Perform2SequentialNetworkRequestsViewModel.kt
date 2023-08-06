a2a91475fd18dbb1243e7d7208650373995f7a17
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-06 19:29:06 +0200

    10. Coroutines Implementation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,12 +1,30 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
+import java.lang.Exception
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch {
+            try {
+                val recentVersion = mockApi.getRecentAndroidVersions()
+                val version = recentVersion.last()
+                val features = mockApi.getAndroidVersionFeatures(version.apiLevel)
+                uiState.value = UiState.Success(features)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("not found")
+            }
+        }
     }
 }
