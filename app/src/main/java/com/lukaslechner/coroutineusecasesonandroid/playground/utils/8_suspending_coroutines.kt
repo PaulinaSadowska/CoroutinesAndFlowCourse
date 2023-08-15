0c3af283b8c235f006a8651b1f4f9fd014ec004e
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-15 17:00:56 +0200

    18. Multithreaded Coroutines

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/utils/8_suspending_coroutines.kt
@@ -0,0 +1,28 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.utils
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    println("main starts ${Thread.currentThread()}")
+
+    joinAll(
+        async { coroutine(1, 500) },
+        async { coroutine(2, 300) },
+        async {
+            repeat(5) {
+                println("working... ${Thread.currentThread()}")
+                delay(100)
+            }
+        }
+    )
+    println("main ends")
+}
+
+private suspend fun coroutine(number: Int, delay: Long) {
+    println("Routine $number starts work! ${Thread.currentThread()}")
+    delay(delay)
+    println("Routine $number has finished")
+}
---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/utils/9_coroutine_different_threads.kt
@@ -0,0 +1,26 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main() = runBlocking {
+    println("main starts ${Thread.currentThread()}")
+
+    joinAll(
+        async { coroutine(1, 500) },
+        async { coroutine(2, 300) },
+    )
+    println("main ends")
+}
+
+private suspend fun coroutine(number: Int, delay: Long) {
+    println("Routine $number starts work! ${Thread.currentThread()}")
+    delay(delay)
+    withContext(Dispatchers.Default) {
+        println("Routine $number has finished  ${Thread.currentThread()}")
+    }
+}
