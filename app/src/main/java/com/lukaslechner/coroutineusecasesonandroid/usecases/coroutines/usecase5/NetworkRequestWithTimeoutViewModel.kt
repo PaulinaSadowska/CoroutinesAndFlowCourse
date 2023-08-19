c2caf007e944fbb1b5ef2827cc123aeb899316b9
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-19 15:01:51 +0200

    Retry and timeout

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,53 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import java.lang.Exception
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        usingWithTimeoutOrNull(timeout)
+    }
 
+    private fun usingWithTimeout(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val result = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(result)
+
+            } catch (e: Exception) {
+                uiState.value = when (e) {
+                    is TimeoutCancellationException -> UiState.Error("timeout")
+                    else -> UiState.Error("something went wrong :(")
+                }
+            }
+        }
+    }
+
+    private fun usingWithTimeoutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                uiState.value = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }?.let {
+                    UiState.Success(it)
+                } ?: run {
+                    UiState.Error("timeout ($timeout)!")
+                }
+            } catch (e: Exception) {
+                UiState.Error("something went wrong :(")
+            }
+        }
     }
 
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,36 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import kotlin.Exception
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
-
+        viewModelScope.launch {
+            try {
+                uiState.value = retry(2) {
+                    api.getRecentAndroidVersions()
+                }.let { UiState.Success(it) }
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("something went wrong :(")
+            }
+        }
     }
 
+    private suspend fun <T> retry(numOfRetries: Int, operation: suspend () -> T): T {
+        repeat(numOfRetries) {
+            try {
+                return operation()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+        return operation()
+    }
 }
