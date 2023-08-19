c09acbe95b0280e58ad3eafaef2597ead4df839f
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-19 15:17:29 +0200

    exponential delay

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,7 +3,9 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import timber.log.Timber
 import kotlin.Exception
 
@@ -14,7 +16,7 @@ class RetryNetworkRequestViewModel(
     fun performNetworkRequest() {
         viewModelScope.launch {
             try {
-                uiState.value = retry(2) {
+                uiState.value = retry(3) {
                     api.getRecentAndroidVersions()
                 }.let { UiState.Success(it) }
             } catch (e: Exception) {
@@ -23,13 +25,22 @@ class RetryNetworkRequestViewModel(
         }
     }
 
-    private suspend fun <T> retry(numOfRetries: Int, operation: suspend () -> T): T {
+    private suspend fun <T> retry(
+        numOfRetries: Int,
+        initialDelay: Long = 100,
+        maxDelayMillis: Long = 1_000,
+        factor: Double = 2.0,
+        operation: suspend () -> T
+    ): T {
+        var currentDelay = initialDelay
         repeat(numOfRetries) {
             try {
                 return operation()
             } catch (e: Exception) {
                 Timber.e(e)
             }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
         }
         return operation()
     }
