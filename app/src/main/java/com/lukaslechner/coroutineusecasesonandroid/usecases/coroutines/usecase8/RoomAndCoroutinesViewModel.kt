3d42ec312e3041ae6eccc24d24d1a13669fe7acc
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-08-19 19:58:14 +0200

    Room

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,15 +12,37 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
+        viewModelScope.launch {
+            uiState.value = database.getAndroidVersions()
+                .takeIf { it.isNotEmpty() }
+                ?.let {
+                    UiState.Success(DataSource.DATABASE, it.mapToUiModelList())
+                } ?: run {
+                UiState.Error(DataSource.DATABASE, "error")
+            }
 
+            try {
+                uiState.value = UiState.Loading.LoadFromNetwork
+                api.getRecentAndroidVersions()
+                    .onEach { database.insert(it.mapToEntity()) }
+                    .let {
+                        uiState.value = UiState.Success(DataSource.NETWORK, it)
+                    }
+            } catch (_: Exception) {
+                uiState.value = UiState.Error(DataSource.NETWORK, "error")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
+
 enum class DataSource(val dataSourceName: String) {
-    DATABASE("Database"),
-    NETWORK("Network")
+    DATABASE("Database"), NETWORK("Network")
 }
